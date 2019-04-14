a024c3dd4022f01183ede390f21db2c1a5fa9d76
Jerry Tsai <purrup27@gmail.com>
2019-04-14 17:15:11 +0800

    Add CRUD design pattern under card and add serch function

--- app.js
+++ app.js
@@ -22,6 +22,19 @@ app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
 
+// render search results
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(
+    restaurant =>
+      // search by name
+      restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
+      // search by category
+      restaurant.category.includes(keyword)
+  )
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
 app.listen(3000, () => {
   console.log(`App is running on http://localhost:3000`)
 })
