c97ec1b3271619a6ee042f245ecb09b4607f3527
Jerry Tsai <purrup27@gmail.com>
2019-04-16 19:28:39 +0800

    Modify edit route with Object.assign()

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.post('/restaurants', (req, res) => {
 app.get('/restaurants/:id', (req, res) => {
   Restaurant.findById(req.params.id, (err, restaurant) => {
     if (err) return console.error(err)
-    return res.render('detail', { restaurant: restaurant })
+    return res.render('detail', { restaurant })
   })
 })
 
@@ -56,7 +56,7 @@ app.get('/restaurants/:id', (req, res) => {
 app.get('/restaurants/:id/edit', (req, res) => {
   Restaurant.findById(req.params.id, (err, restaurant) => {
     if (err) return console.error(err)
-    return res.render('edit', { restaurant: restaurant })
+    return res.render('edit', { restaurant })
   })
 })
 
@@ -64,15 +64,7 @@ app.get('/restaurants/:id/edit', (req, res) => {
 app.post('/restaurants/:id/', (req, res) => {
   Restaurant.findById(req.params.id, (err, restaurant) => {
     if (err) return console.error(err)
-    restaurant.name = req.body.name
-    restaurant.name_en = req.body.name_en
-    restaurant.category = req.body.category
-    restaurant.image = req.body.image
-    restaurant.location = req.body.location
-    restaurant.phone = req.body.phone
-    restaurant.google_map = req.body.google_map
-    restaurant.rating = req.body.rating
-    restaurant.description = req.body.description
+    Object.assign(restaurant, req.body)
 
     restaurant.save(err => {
       if (err) return console.error(err)
