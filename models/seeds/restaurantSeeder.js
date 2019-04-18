a3d69c4fb5ffa1aadd1629d8cba9c062a855597a
Jerry Tsai <purrup27@gmail.com>
2019-04-18 20:22:31 +0800

    shorten create seeds method with let ...of in the restaurnatSeeders

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -10,19 +10,8 @@ db.on('error', () => {
 
 db.once('open', () => {
   console.log('db connected!')
-
-  for (i = 0; i < restaurantList.results.length; i++) {
-    Restaurant.create({
-      name: restaurantList.results[i].name,
-      name_en: restaurantList.results[i].name_en,
-      category: restaurantList.results[i].category,
-      image: restaurantList.results[i].image,
-      location: restaurantList.results[i].location,
-      phone: restaurantList.results[i].phone,
-      google_map: restaurantList.results[i].google_map,
-      rating: restaurantList.results[i].rating,
-      description: restaurantList.results[i].description,
-    })
+  for (let restaurant of restaurantList.results) {
+    Restaurant.create(restaurant)
   }
   console.log('done!')
 })
