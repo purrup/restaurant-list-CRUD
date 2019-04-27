9aa0d1720657d4bb51bbcaf7173e8aa824a6dbf0
Jerry Tsai <purrup27@gmail.com>
2019-04-28 00:05:19 +0800

    create user.json, connect seeds of users and  seeds of restaurants with userId

--- config/passport.js
+++ config/passport.js
@@ -15,7 +15,9 @@ module.exports = passport => {
         bcrypt.compare(password, user.password, (err, isMatch) => {
           if (err) throw err
           if (isMatch) {
-            return done(null, user)
+            return done(null, user, {
+              message: '登入',
+            })
           } else {
             return done(null, false, {
               message: 'Password is incorrect',
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose')
 const Restaurant = require('../restaurant')
-const restaurantList = require('../../restaurant.json')
-mongoose.connect('mongodb://localhost/restaurants', { useNewUrlParser: true })
+const restaurantList = require('../../restaurant.json').results
+const userList = require('../../user.json')
+const User = require('../user')
+const bcrypt = require('bcryptjs')
+const users = userList.results
+
+mongoose.connect('mongodb://localhost/restaurants', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+})
 const db = mongoose.connection
 
 db.on('error', () => {
@@ -10,8 +18,25 @@ db.on('error', () => {
 
 db.once('open', () => {
   console.log('db connected!')
-  for (let restaurant of restaurantList.results) {
-    Restaurant.create(restaurant)
+  for (let i = 0; i < users.length; i++) {
+    const user = User(users[i])
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash
+        user.save()
+      })
+    })
+    // 一個使用者分配三間餐廳
+    for (let j = i * 3; j < i * 3 + 3; j++) {
+      // i = 0, j = 0, j< 3
+      // i = 0, j = 1, j < 3
+      // i = 0, j = 2, j < 3
+      // i = 1, j = 3, j < 6
+      // i = 1, j = 4, j < 6
+      // i = 1, j = 5, j < 6
+      Restaurant.create({ ...restaurantList[j], userId: user._id })
+    }
   }
+
   console.log('done!')
 })
